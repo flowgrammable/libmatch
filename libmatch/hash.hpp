5a866930fe2671011abc3075b286acc09d0761fc
mgruesen <mgruesen7@gmail.com>
2015-06-11 11:19:13 -0400

    cleaned up bucket types, moved iterator to linear class

--- libmatch/hash.hpp
+++ libmatch/hash.hpp
@@ -121,152 +121,54 @@ template<typename T>
 class basic_bucket
 { 
 public:
+  // Constructors
+  basic_bucket();
+  basic_bucket(T const& v);
+  basic_bucket(T& v);
 
-  class iterator
-  {
-    using data_type = basic_bucket<T>*;
-    using reference_type = boost::optional<T>&;
-    using pointer_type = boost::optional<T>*;
-  public:
-    // Constructor/Destructor
-    iterator(data_type);
-    ~iterator();
-
-    // Assignment and Relational operators.
-    iterator& operator=(iterator const&);
-    bool operator==(iterator const&) const;
-    bool operator!=(iterator const&);
-
-    // Prefix increment
-    iterator& operator++();
-    // Postfix increment
-    iterator& operator++(int);
-
-    // Dereferencing operations
-    reference_type operator*();
-    pointer_type operator->();
-
-  private:
-    data_type data_;
-  };
-
-
-  basic_bucket()
-  { }
-
-  basic_bucket(T const& v)
-    : data_(v)
-  { }
-
-  basic_bucket(T& v)
-    : data_(std::move(v))
-  { }
 
   // Accessors
   bool is_full() const;
   bool is_empty() const;
-
-  iterator begin();
-  iterator end();
   
   T&       get();
   T const& get() const;
 
+
   // Mutators
-  iterator& insert(T const&);
-  iterator& insert(T&&);
+  void insert(T const&);
+  void insert(T&&);
 
   template<typename... Args> 
-  iterator& emplace(Args&&...);
+  void emplace(Args&&...);
   
   void erase(T const&);
   void clear();
 
 private:
+  // Data members
   boost::optional<T> data_;
 };
 
 
-// Constructor
+// Basic bucket default constructor.
 template<typename T>
-basic_bucket<T>::iterator::iterator(data_type data = 0)
-  : data_(data)
+basic_bucket<T>::basic_bucket()
 { }
 
 
-// Destructor
+// Basic bucket copy constructor.
 template<typename T>
-basic_bucket<T>::iterator::~iterator()
-{
-  delete data_;
-}
+basic_bucket<T>::basic_bucket(T const& v)
+  : data_(v)
+{ }
 
 
-// Assignment operator.
+// Basic bucket move constructor.
 template<typename T>
-auto
-basic_bucket<T>::iterator::operator=(iterator const& other) -> iterator&
-{
-  data_ = other.data_;
-  return *this;
-}
-
-
-// Equality operator
-template<typename T>
-inline bool 
-basic_bucket<T>::iterator::operator==(iterator const& other) const
-{
-  return (data_ == other.data_);
-}
-
-
-// Inequality operator
-template<typename T>
-inline bool 
-basic_bucket<T>::iterator::operator!=(iterator const& other)
-{
-  return !(data_ == other.data_);
-}
-
-
-// Prefix increment.
-template<typename T>
-auto
-basic_bucket<T>::iterator::operator++() -> iterator& 
-{
-  data_++;
-  return *this;
-}
-
-
-// Postfix increment.
-template<typename T>
-auto
-basic_bucket<T>::iterator::operator++(int) -> iterator&
-{
-  iterator temp = *this;
-  data_++;
-  return *temp;
-}
-
-
-// Dereferencing operator.
-template<typename T>
-auto
-basic_bucket<T>::iterator::operator*() -> reference_type
-{
-  return *data_;
-}
-
-
-// Dereferencing operator.
-template<typename T>
-auto
-basic_bucket<T>::iterator::operator->() -> pointer_type
-{
-  return data_;
-}
+basic_bucket<T>::basic_bucket(T& v)
+  : data_(std::move(v))
+{ }
 
 
 // Retrieves the current value from the bucket. The bucket
@@ -311,23 +213,21 @@ basic_bucket<T>::is_empty() const
 
 // Copies `t` to the bucket. The bucket shall not be full.
 template<typename T>
-inline auto 
-basic_bucket<T>::insert(T const& t) -> iterator&
+inline void 
+basic_bucket<T>::insert(T const& t)
 {
   assert(is_empty());
   data_ = t;
-  return iterator(this);
 }
 
 
 // Moves `t` into the bucket. The bucket shall not be full.
 template<typename T>
-inline auto 
-basic_bucket<T>::insert(T&& t) -> iterator&
+inline void 
+basic_bucket<T>::insert(T&& t)
 {
   assert(is_empty());
   data_ = std::move(t);
-  return iterator(this);
 }
 
 
@@ -335,11 +235,10 @@ basic_bucket<T>::insert(T&& t) -> iterator&
 // The bucket shall not be full.
 template<typename T>
 template<typename... Args>
-inline auto
-basic_bucket<T>::emplace(Args&&... args) -> iterator&
+inline void
+basic_bucket<T>::emplace(Args&&... args)
 {
   data_.emplace(std::forward<Args>(args)...);
-  return iterator(this);
 }
 
 
@@ -363,55 +262,32 @@ basic_bucket<T>::clear()
 }
 
 
-// Creates an iterator pointing to the bucket.
-template<typename T>
-inline auto
-basic_bucket<T>::begin() -> iterator
-{
-  return iterator(this);
-}
-
-
-// Creates an iterator pointing past the bucket.
-template<typename T>
-inline auto
-basic_bucket<T>::end() -> iterator
-{
-  return iterator();
-}
-
-
 // A chained hash table bucket containing chained entries.
 template<typename T>
 class list_bucket 
 {
 public:
+  // Constructors/Destructor
   list_bucket();
   list_bucket(T const& v);
   ~list_bucket();
 
   // Accessors
+  bool is_empty() const;
+  
+  T&       get();
+  T const& get() const;
 
-  
-  // Retrieves the entry from the bucket.
-  boost::optional<T> open();
 
-  
-  // Checks if the bucket contains any entries.
-  inline bool is_empty();
-  
-  
   // Mutators
+  void insert(T const&);
+  void insert(T&&);
 
+  template<typename... Args> 
+  void emplace(Args&&...);
   
-  // Adds an entry to the bucket. Since this is an unbounded data store
-  // type there is no concept of being 'full'.
-  void fill(T const& v);
-
-
-  // Clears the bucket of its values.
-  void
-  dump();
+  void erase(T const&);
+  void clear();
 
 private:
   // Data Members
@@ -421,77 +297,34 @@ private:
 
 // An array based hash table bucket.
 template<typename T>
-struct array_bucket
+class array_bucket
 {
+public:
+  // Constructors/Destructor
+  array_bucket();
+  array_bucket(int);
+  ~array_bucket();
+
   
-  array_bucket()
-    : array_bucket(10)
-  { }
-
-  array_bucket(int n)
-    : size(0), capacity(n)
-  { 
-    data_ = new boost::optional<T>[n];
-  }
-
-  ~array_bucket()
-  {
-    delete data_;
-  }
-
-
-  // Accesors
-
-
-  // Retrieves the entries from the bucket.
-  boost::optional<T>*
-  open()
-  {
-    return (is_empty() ? boost::none : data_);
-  }
-
-
-  // Checks if the bucket is at maximum capacity.
-  inline bool
-  is_full()
-  {
-    return size == capacity - 1;
-  }
-
-
-  // Checks if the bucket contains any entries.
-  inline bool
-  is_empty()
-  {
-    return size;
-  }
+  // Accessors
+  bool is_full() const;
+  bool is_empty() const;
+  
+  T&       get();
+  T const& get() const;
 
 
   // Mutators
+  void insert(T const&);
+  void insert(T&&);
 
+  template<typename... Args> 
+  void emplace(Args&&...);
+  
+  void erase(T const&);
+  void clear();
 
-  // Adds an entry if the bucket is not full. Since this is a bounded
-  // data store type there is a maximum number of entries that can be
-  // held.
-  void
-  fill(T const& v)
-  {
-    if (size + 1 < capacity) {
-      data_[size++].emplace(v);
-    }
-  }
-
-
-  // Clears the bucket of its entries.
-  void
-  dump()
-  {
-    delete data_;
-    data_ = new boost::optional<T>[capacity];
-    size = 0;
-  }
-
-
+private:
   // Data Members
   boost::optional<T>* data_;  // Data store
   int size;                   // Current size
@@ -502,64 +335,33 @@ struct array_bucket
 // A tree based hash table bucket.
 // TODO: implement this using an array, not pointers
 template<typename T>
-struct tree_bucket
+class tree_bucket
 { 
-  tree_bucket()
-  { }
-
-  tree_bucket(T const& v)
-  { 
-    data_.emplace(v);
-  }
-
-  ~tree_bucket()
-  {
-    // TODO: Same issue as dump().
-    data_ = boost::none;
-  }
+public:
+  //Constructors/Destructor
+  tree_bucket();
+  tree_bucket(T const& v);
+  ~tree_bucket();
 
 
   // Accessors
-
-
-  // Retrieves the entries from the bucket.
-  boost::optional<T>
-  open()
-  {
-    return (is_empty() ? boost::none : *data_);
-  }
-
-
-  // Checks if the bucket contains any entries.
-  inline bool
-  is_empty()
-  {
-    return data_ == boost::none;
-  }
+  bool is_empty() const;
   
+  T&       get();
+  T const& get() const;
+
 
   // Mutators
+  void insert(T const&);
+  void insert(T&&);
 
-
-  // Adds an entry to the bucket.
-  void
-  fill(T const& v)
-  {
-    // TODO: balance & sort the tree when adding to it
-    data_.emplace(v);
-  }
-
+  template<typename... Args> 
+  void emplace(Args&&...);
   
-  // Clears the bucket of any entries.
-  void
-  dump()
-  {
-    // TODO: Same issue that list_bucket has. Will need to traverse
-    // the tree and delete nodes.
-    data_ = boost::none;
-  }
+  void erase(T const&);
+  void clear();
 
-
+private:
   // Data Members
   boost::optional<T> data_;
 };
@@ -582,6 +384,34 @@ public:
   using value_type = data_store::basic_bucket<data_store::entry<K, V>>;
   using hasher = H;
   using compare = C;  
+
+  class iterator
+  {
+    using data_type = linear::value_type*;
+    using reference_type = linear::value_type&;
+    using pointer_type = linear::value_type*;
+  public:
+    // Constructor/Destructor
+    iterator(data_type);
+    ~iterator();
+
+    // Assignment and Relational operators.
+    iterator& operator=(iterator const&);
+    bool operator==(iterator const&) const;
+    bool operator!=(iterator const&);
+
+    // Prefix increment
+    iterator& operator++();
+    // Postfix increment
+    iterator& operator++(int);
+
+    // Dereferencing operations
+    reference_type operator*();
+    pointer_type operator->();
+
+  private:
+    data_type data_;
+  };
 
   linear();
   linear(int);
@@ -697,6 +527,88 @@ inline int
 linear<K, V, H, C>::get_hash_index(K const& key) const
 { 
   return hash_(key) % buckets; 
+}
+
+
+// Constructor
+template<typename K, typename V, typename H, typename C> 
+linear<K, V, H, C>::iterator::iterator(data_type data = 0)
+  : data_(data)
+{ }
+
+
+// Destructor
+template<typename K, typename V, typename H, typename C> 
+linear<K, V, H, C>::iterator::~iterator()
+{
+  delete data_;
+}
+
+
+// Assignment operator.
+template<typename K, typename V, typename H, typename C> 
+auto
+linear<K, V, H, C>::iterator::operator=(iterator const& other) -> iterator&
+{
+  data_ = other.data_;
+  return *this;
+}
+
+
+// Equality operator
+template<typename K, typename V, typename H, typename C> 
+inline bool 
+linear<K, V, H, C>::iterator::operator==(iterator const& other) const
+{
+  return (data_ == other.data_);
+}
+
+
+// Inequality operator
+template<typename K, typename V, typename H, typename C> 
+inline bool 
+linear<K, V, H, C>::iterator::operator!=(iterator const& other)
+{
+  return !(data_ == other.data_);
+}
+
+
+// Prefix increment.
+template<typename K, typename V, typename H, typename C> 
+auto
+linear<K, V, H, C>::iterator::operator++() -> iterator& 
+{
+  data_++;
+  return *this;
+}
+
+
+// Postfix increment.
+template<typename K, typename V, typename H, typename C> 
+auto
+linear<K, V, H, C>::iterator::operator++(int) -> iterator&
+{
+  iterator temp = *this;
+  data_++;
+  return *temp;
+}
+
+
+// Dereferencing operator.
+template<typename K, typename V, typename H, typename C> 
+auto
+linear<K, V, H, C>::iterator::operator*() -> reference_type
+{
+  return *data_;
+}
+
+
+// Dereferencing operator.
+template<typename K, typename V, typename H, typename C> 
+auto
+linear<K, V, H, C>::iterator::operator->() -> pointer_type
+{
+  return data_;
 }
 
 
