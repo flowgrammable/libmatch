ce0d2063bef43ae402f07bca7e27745b7221fec4
Naveen Thomas <naveengt1989@gmail.com>
2015-06-29 14:29:20 -0500

    Update stride_trie.cpp
    Format changes

--- test/stride_trie.cpp
+++ test/stride_trie.cpp
@@ -5,7 +5,6 @@
 #include <utility> 
 #include <memory.h>
 #include "../libmatch/stride_trie.hpp"
-using namespace std;
 
 enum OPR
 {
@@ -18,57 +17,67 @@ enum OPR
 // Format: 
 // 1st line indicates T(number of operations)
 // Followed by T lines
-// If insertion operation, line contains: <Op type: 1> <RULE DATA(HEXA FORMAT)> <PREFIX LENGTH> <NEXT_HOP INFO> 
-// If search operation, line contains: <Op type: 2> <INPUT DATA(HEXA FORMAT)>  
-// If update operation, line contains: <Op type: 4> <RULE DATA(HEXA FORMAT)> <PREFIX LENGTH> <NEXT_HOP INFO> 
-// If delete operation, line contains: <Op type: 3> <RULE DATA(HEXA FORMAT)> <PREFIX LENGTH>  
+// If insertion operation, 
+// line contains: <Op type: 1> <RULE DATA(HEXA FORMAT)> 
+//                <PREFIX LENGTH> <NEXT_HOP INFO> 
+// If search operation,
+// line contains: <Op type: 2> <INPUT DATA(HEXA FORMAT)>  
+// If update operation,
+// line contains: <Op type: 4> <RULE DATA(HEXA FORMAT)>
+//		  <PREFIX LENGTH> <NEXT_HOP INFO> 
+// If delete operation,
+// line contains: <Op type: 3> <RULE DATA(HEXA FORMAT)> <PREFIX LENGTH>  
 int main(int argc, char* argv[])
 {
-	trie_node<int,UL,string>* root= new trie_node<int,UL,string>(); 	
+	trie_node<int,ul,std::string>* root= new trie_node<int,ul,std::string>(); 	
 	root->init(3);
-	MAX_PREFIX_LENGTH = 32;	
+	// setting maximum number of bits a rule can contain
+	max_prefix_length = 32;	
 
 	int T;
-	cin>>T;
+	std::cin>>T;
 
-	while(T--)
+	while (T--)
 	{
 		int type;
-		cin>>type;
-		if((OPR)type == INSERTION)
+		std::cin>>type;
+		if ((OPR)type == INSERTION)
 		{
-			UL prefix; int prefix_length; string next_hop;
-			cin>>hex>>prefix;
-			cin>>prefix_length;
-			cin>>next_hop;
-			root->insertData(root, prefix, prefix_length, next_hop);
+			ul prefix; int prefix_length; std::string next_hop;
+			std::cin>>std::hex>>prefix;
+			std::cin>>prefix_length;
+			std::cin>>next_hop;
+			root->insertData(root, prefix, prefix_length, 
+			next_hop);
 		}
-		else if((OPR)type == SEARCH)
+		else if ((OPR)type == SEARCH)
 		{
-			UL item_to_be_searched;
-			prefix_node<string>* BMP=NULL;
-			cin>>hex>>item_to_be_searched;
+			ul item_to_be_searched;
+			prefix_node<std::string>* BMP=NULL;
+			std::cin>>std::hex>>item_to_be_searched;
 			root->searchData(root, item_to_be_searched, BMP);	
 			if(BMP)
-				cout<<"NEXT HOP found: "<<BMP->getHop()<<"\n";
+				std::cout<<"NEXT HOP found: "<<BMP->getHop()<<"\n";
 			else
-				cout<<"No Matching Rule Found!!\n";
+				std::cout<<"No Matching Rule Found!!\n";
 		}
-		else if((OPR)type == UPDATE)
+		else if ((OPR)type == UPDATE)
 		{
-			UL prefix; int prefix_length; string next_hop;
-			cin>>hex>>prefix;
-			cin>>prefix_length;
-			cin>>next_hop;
+			ul prefix; int prefix_length; std::string next_hop;
+			std::cin>>std::hex>>prefix;
+			std::cin>>prefix_length;
+			std::cin>>next_hop;
 			root->updateData(root, prefix, prefix_length, next_hop);
 		}
-		else if((OPR)type == DELETION)
+		else if ((OPR)type == DELETION)
 		{
-			UL prefix; int prefix_length;;
-			cin>>hex>>prefix;
-			cin>>prefix_length;
+			ul prefix; int prefix_length;
+			std::cin>>std::hex>>prefix;
+			std::cin>>prefix_length;
 			root->deleteData(root, prefix, prefix_length);
 		}
 	}
 	return 0;
 }
+
+
