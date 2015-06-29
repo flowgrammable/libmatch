b386ba227c0c6d02ccf02aa99e905623c7bcd541
Michael Gruesen <mgruesen7@gmail.com>
2015-06-29 09:27:07 -0400

    Styling and brief code review

--- libmatch/stride_trie.hpp
+++ libmatch/stride_trie.hpp
@@ -6,12 +6,23 @@
 #include <cmath>
 #include <algorithm>
 #include <utility> 
-#include <memory.h>
+#include <cstring>
 
+// FIXME: Don't use 'using namespace std' We may have conflicting types
+// with the standard library so we want to make sure we know what the
+// fully qualified name is.
 using namespace std;
+
+// FIXME: For C, this would be a typedef, not a define since you are
+// defining a type. For C++ the preferred way to do this is a 'using'
+// directive. (ex. using ul = unsigned long int;)
 #define UL unsigned long int
 //#define MAX_PREFIX_LENGTH 32
+// FIXME: Do not to use all caps for variable names.
 int MAX_PREFIX_LENGTH;
+
+// FIXME: Try to keep the column width at '70'
+
 /////////////////////////////////////////////////////// Description //////////////////////////////////////////////////////////////////
 // Program uses a tree bitmap scheme for achieving optimum storage and search speed for storing prefixes, along with fast insertions.  
 // In this scheme, there are 2 bitmaps stored per trie node, one for all internally stored prefixes and one for the external pointers.
@@ -20,31 +31,58 @@ int MAX_PREFIX_LENGTH;
 // contain the next Hop information associated with a particular prefix rule. For details, refer: Network Algorithmics: Section- 11.8 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// FIXME: Don't use all caps for data structure names. Also, keeping
+// with the style of the other parts of this library move the definitions
+// of functions outside the data structure, unless its trivial.
+
 // Bitmap where 1 bit indicate valid/invalid status. 
 template<typename T>
 struct BITMAP
 {
+	// FIXME: this is not a type, refrain from using the postfix _t.
+	// Maybe try 'using bitfield_t = unsigned long;' then have this
+	// member look like 'bitfield_t* bitfield;' You can then also use
+	// the bitfield_t instead of writing out 'unsigned long' everywhere.
 	unsigned long* bfield_t;
-	void init(size_t size)
+
+
+	// NOTE: Use two spaces between functions, and leave the return type
+	// hanging above the function declaration
+
+	// FIXME: Make this a constructor
+	void 
+	init(size_t size)
 	{
 		size_t mem_cells = ceil((double)size/(double)sizeof(unsigned long)) ; 
-		bfield_t = new unsigned long[mem_cells]; 
+		bfield_t = new unsigned long[mem_cells];
 		memset(bfield_t, 0, sizeof(unsigned long)*mem_cells);
 	}
-	T get(T index)
+
+	// FIXME: use a destructor to handle memory cleanup
+
+	T 
+	get(T const& index)
 	{
 		return 1 & (bfield_t[index/(8*sizeof(unsigned long))]>> (index % (8 * sizeof(unsigned long) )));
 	}
-	void set(T index)
+
+
+	void 
+	set(T const& index)
 	{
 		bfield_t[index/(8*sizeof(unsigned long))] |= (1<< (index % (8 * sizeof(unsigned long) )));
 	}
-	void reset(T index)
+	
+
+	void 
+	reset(T const& index)
 	{
 		bfield_t[index/(8*sizeof(unsigned long))] &= ~(1<< index % (8 * sizeof(unsigned long) ));	
 	}
 
-	bool checkIfEmpty(T size)	
+	
+	bool 
+	checkIfEmpty(T const& size) const
 	{
 		T mem_cells = ceil((double)size/(double)sizeof(unsigned long)) ;
 		T i;
@@ -63,12 +101,16 @@ class prefix_node
 {
 public:
 	V next_hop;
-	
+
+	// FIXME: Use a constructor instead of this 'init' function	
 	void init(V data)
 	{
 		next_hop = data;
 	}
-	V getHop()
+
+
+	V 
+	getHop()
 	{
 		return next_hop;
 	}
@@ -86,6 +128,8 @@ class trie_node
 	prefix_node<V>* prefixNodes;
 	
 public:
+	// FIXME: use a constructor
+	// FIXME: use a destructor to handle memory cleanup
 	void init(G);	
 	bool checkIfEmpty();
 	T getStride();
@@ -109,7 +153,8 @@ void trie_node<T,G,V>::init(G strd)
 template <typename T, typename G, typename V>
 bool trie_node<T,G,V>::checkIfEmpty()
 {
-	if(triePointers.checkIfEmpty((G)pow(2,stride)) && prefixPointers.checkIfEmpty((G)pow(2,stride)*2-1))	
+	// FIXME: put a space between 'for', 'while', and 'if' and the first '('
+	if (triePointers.checkIfEmpty((G)pow(2,stride)) && prefixPointers.checkIfEmpty((G)pow(2,stride)*2-1))	
 	{
 		delete[] trieNodes;
 		delete[] prefixNodes;
@@ -129,17 +174,17 @@ T trie_node<T,G,V>::getStride()
 template <typename T, typename G, typename V>
 void trie_node<T,G,V>::insertData(trie_node<T,G,V>* node, G prefix, T prefix_length, V next_hop)
 {
-	while(1)
+	while (1)
 	{
 		G mask = ~0 << (MAX_PREFIX_LENGTH - min(node->stride,prefix_length));
 		G node_prefix = (prefix&mask)>>(MAX_PREFIX_LENGTH - min(node->stride, prefix_length));	
 
 		// update prefixes in the corresponding trie positions
-		if(prefix_length > node->stride)	
+		if (prefix_length > node->stride)	
 		{
 			trie_node<T,G,V>* tn = node->trieNodes + node_prefix;
 			// create trie node pointer	
-			if(!node->triePointers.get(node_prefix))
+			if (!node->triePointers.get(node_prefix))
 			{
 				cout<<"Initializing trie node: "<<node<<"\n";
 				tn->init(node->stride);
@@ -167,17 +212,17 @@ void trie_node<T,G,V>::insertData(trie_node<T,G,V>* node, G prefix, T prefix_len
 template <typename T, typename G, typename V>
 void trie_node<T,G,V>::searchData(trie_node<T,G,V>* node, G item, prefix_node<V>*& BMP)
 {
-	while(1)
+	while (1)
 	{
 		cout<<"Traversing trie node: "<<node<<"\n";
 		G mask = ~0 << (MAX_PREFIX_LENGTH - node->stride);
 		G node_prefix = (item&mask) >>  (MAX_PREFIX_LENGTH - stride);
 		T cur_stride= stride;
 		// update BMP
-		while(cur_stride > 0)
+		while (cur_stride > 0)
 		{
 			T prefixNodeIndex = (pow(2, cur_stride)-1) + (node_prefix >> (stride - cur_stride));
-			if(prefixPointers.get(prefixNodeIndex))
+			if (prefixPointers.get(prefixNodeIndex))
 			{
 				cout<<"Setting Prefix Node: "<<prefixNodes + prefixNodeIndex<<" index: "<<prefixNodeIndex<<"\n";
 				BMP = prefixNodes + prefixNodeIndex;
@@ -187,7 +232,7 @@ void trie_node<T,G,V>::searchData(trie_node<T,G,V>* node, G item, prefix_node<V>
 		}
 		// traverse successor trie node if it exists
 		// create trie node pointer	
-		if(triePointers.get(node_prefix))
+		if (triePointers.get(node_prefix))
 		{
 			item = item<<node->stride;
 			node = trieNodes + node_prefix;
@@ -202,16 +247,16 @@ void trie_node<T,G,V>::searchData(trie_node<T,G,V>* node, G item, prefix_node<V>
 template <typename T, typename G, typename V>
 void trie_node<T,G,V>::updateData(trie_node<T,G,V>* node, G prefix, T prefix_length, V next_hop)
 {
-	while(1)
+	while (1)
 	{
 		G mask = ~0 << (MAX_PREFIX_LENGTH - min(node->stride,prefix_length));
 		G node_prefix = (prefix&mask)>>(MAX_PREFIX_LENGTH - min(node->stride, prefix_length));	
 
-		if(prefix_length > node->getStride())	
+		if (prefix_length > node->getStride())	
 		{
 			trie_node<T,G,V>* tn = node->trieNodes + node_prefix;
 			// create trie node pointer	
-			if(!node->triePointers.get(node_prefix))
+			if (!node->triePointers.get(node_prefix))
 			{
 				cout<<"Error !!! Prefix Not Found\n";
 				break;
@@ -228,7 +273,7 @@ void trie_node<T,G,V>::updateData(trie_node<T,G,V>* node, G prefix, T prefix_len
 			// update prefix pointer
 			T prefixPointerIndex = pow(2, prefix_length) -1 + node_prefix;
 			prefix_node<V>* pn = node->prefixNodes + prefixPointerIndex;
-			if(node->prefixPointers.get(prefixPointerIndex))
+			if (node->prefixPointers.get(prefixPointerIndex))
 			{
 				cout<<"Updating Prefix node: "<<pn<<" with next hop info: "<<next_hop<<"\n";
 				pn->init(next_hop);
