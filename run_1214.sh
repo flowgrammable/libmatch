55f7de6f84e66f5368dec1a919fae3d6df5b02e2
Ping Wang <pingwang.tamu@gmail.com>
2016-12-14 11:33:44 -0600

    updated run script file

--- run_1214.sh
+++ run_1214.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "=====2925====="
 ./optimized_libmatch/test/group_search_reviewed.test ~/libmatch/test/Benchmarks/ping_big_64bit_original.txt ~/libmatch/test/Benchmarks/two_fields_64bit.txt ~/libmatch/test/Benchmarks/action2925.txt 18 >> nolimit.txt
-
+./optimized_libmatch/test/group_noexpand_base.test ~/libmatch/test/Benchmarks/ping_big_64bit_original.txt ~/libmatch/test/Benchmarks/two_fields_64bit.txt ~/libmatch/test/Benchmarks/action2925.txt >> nolimit.txt
+./optimized_libmatch/test/group_advanced.test ~/libmatch/test/Benchmarks/ping_big_64bit_original.txt ~/libmatch/test/Benchmarks/two_fields_64bit.txt ~/libmatch/test/Benchmarks/action2925.txt 2000000 >> result_try1.txt
 ./optimized_libmatch/test/group_nolimit.test ~/libmatch/test/Benchmarks/ping_big_64bit_original.txt ~/libmatch/test/Benchmarks/two_fields_64bit.txt ~/libmatch/test/Benchmarks/action2925.txt >> nolimit.txt
 
